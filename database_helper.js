22ddd2b0a0eac6d9541eb235e81990214fcbd2c4
Kaish02 <kkaish845@email.com>
2025-10-05 17:52:45 +0530

    chages

--- database_helper.js
+++ database_helper.js
@@ -1,17 +1,21 @@
-const mysql2DB=require('mysql2');
-const pool=mysql2DB.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'user_auth_api'
+// database_helper.js
+require('dotenv').config();  // dotenv load kar do
+
+const mysql2DB = require('mysql2');
+
+const pool = mysql2DB.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-pool.connect((err)=>{
-    if(err){
-        console.error('Database connection failed: ',err);
+pool.connect((err) => {
+    if (err) {
+        console.error('Database connection failed: ', err);
         return;
     }
-    console.log('Connected to the database.');
+    console.log(' Connected to the database.');
 });
 
-module.exports=pool;
+module.exports = pool;
